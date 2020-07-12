708bc68faf9fdec187cd0e55f0c70f85989c98e7
prem <prem.sherpa@codeimmersives.com>
2020-07-12 13:29:40 -0400

    done----

--- main.js
+++ main.js
@@ -30,61 +30,77 @@ const command = process.argv[2];
 // the name they want it run on
 const name = process.argv[3]
 // the corresponding client
-const client = clients[names.indexOf(name) !== -1]
+const client = clients[names.indexOf(name)]//removed -1
 
 
 // get a random client from whatever list was passed in
 const randomClient = function(clients) {
-  return clients[Math.floor(Math.random() * clients.length - 1)];
+  return clients[Math.floor(Math.random() * clients.length)];//removed -1
 }
 
-const matchRandomly = function(client) {
+const matchRandomly = function() {
   // get our client's location within our system
-  const clientLocation = clients.indexOf(client);
-
+  const clientLocation = clients.indexOf(clients[Math.ceil(Math.random()*clients.length-1)+1]);
   // exclude our client from matches by making an array of everyone else
   // find all the clients before our client in the system
-  const clientsBeforeOurClient = clients.slice(0, clientLocation);
+  const clientsBeforeOurClient = clients.slice(0,clientLocation);
   // find all the clients after our client in the system
-  const clientsAfterOurClient = clients.slice(clientLocation);
+  const clientsAfterOurClient = clients.slice(clientLocation)//
   // add them together
-  const otherClients = clientsBeforeOurClient + clientsAfterOurClient;
-
+  const otherClients = clientsBeforeOurClient[clientsBeforeOurClient.length-1]+ ' and '+clientsAfterOurClient[0];//changes here, look at next line
+  //before   const otherClients = clientsBeforeOurClient + clientsAfterOurClient; after is above
   // return a random client from the remaining pool
-  return randomClient(otherClients);
+  return `The random good matches " ${otherClients}  ."  `
+
 }
+// console.log(matchRandomly());
 
 const getRank = function(client) {
   // this is backwards or something? they're supposed to be ranked
   // from lowest to highest, and the top one (spider, obvously) should
   // be ranked #1
-  return clients.indexOf(client);
+  let newClients = [];
+  for ( let i = 0; i< clients.length;i++){//loop through to get the ranking from backward
+    newClients.unshift(clients[i])
+  }
+
+  return `The rank of ${client}  is: ${newClients.indexOf(client)+1}`;
 }
+// console.log(getRank('ant'))
 
 const getMatch = function(client) {
   // get the client's location in our data
   const clientLocation = clients.indexOf(client);
-
+// console.log(clientLocation)
   // find their two nearest neighbors
   const neighbor1 = clients[clientLocation - 1];
   const neighbor2 = clients[clientLocation + 1];
   const neighbors = [neighbor1, neighbor2];
-
+  
   // pick one of them and return it
-  return matchRandomly(neighbors);
+  if ( neighbors[0] && !neighbors[1]){// to not get the undefined value when there is only one match for the first or last rank
+    return `it's the only match ${neighbors[0]}`;
+  } else if ( !neighbors[0] && neighbors[1]){
+    return `it's the only match ${neighbors[1]}`;
+  } else {
+
+    return `Heres the choice ${neighbor1} and ${neighbor2} and the random match is ${neighbors[Math.floor(Math.random() * neighbors.length)]}`
+  }
+  
 }
 
 
-if (command === 'random') {
+
+if (command === 'random' && !name) {//added !name
   // match them randomly
   console.log(matchRandomly(client));
-} else if (command === 'rate') {
+} else if (command === 'rate' && name) {//added name
   // get back their rank in the system
   console.log(getRank(client));
-} else if (command === 'match') {
+} else if (command === 'match' && name) {//added name
   // get one of their neighbors in the ranking
   console.log(getMatch(client));
-} else if (command !== 'match') {
+} else  {
   console.log('Please try one of our options:');
   console.log('random [client name] -> a totally random other user');
   console.log('match [client name] -> a match of similar ranking');
